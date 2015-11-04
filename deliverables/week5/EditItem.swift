e347154d8879a16e633f9fb3654bff57ca2771b0
Aliisa Roe <aliisakroe@gmail.com>
2015-11-04 01:44:39 -0700

    Not finished

---
+++ deliverables/week5/EditItem.swift
@@ -0,0 +1,91 @@
+//
+//  EditItem.swift
+//  week5
+//
+//  Created by Aliisa Roe on 11/1/15.
+//  Copyright © 2015 Spark Boulder. All rights reserved.
+//
+
+import UIKit
+
+
+class EditItem: UIViewController, UIPickerViewDelegate {
+    
+    var delegate : EditItemDelegate? = nil
+    
+    
+    var itemNameLabelText : String!
+    var itemQuantityLabelText : String!
+    var itemCategoryLabelText : String!
+    var categories : [String]!
+    var chosenCategory: String!
+    var editedItemName : String!
+    var editedItemQuantity : Int!
+
+
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemQuantityLabel: UILabel!
+    @IBOutlet weak var itemCategoryLabel: UILabel!
+    @IBOutlet weak var itemNameTextField: UITextField!
+    @IBOutlet weak var itemQuantityTextField: UITextField!
+    @IBOutlet weak var categoryPicker: UIPickerView!
+
+
+    @IBAction func editName(sender: AnyObject) {
+        categoryPicker.resignFirstResponder()
+        itemNameTextField.hidden = false
+    }
+    @IBAction func editQuantity(sender: AnyObject) {
+        itemQuantityTextField.hidden = false
+    }
+    @IBAction func editCategory(sender: AnyObject) {
+        categoryPicker.hidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        itemNameLabel.text = itemNameLabelText
+        itemQuantityLabel.text = itemQuantityLabelText
+        itemCategoryLabel.text = itemCategoryLabelText
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return categories.count
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done(sender: AnyObject) {
+        chosenCategory = categoryPicker.selectedRowInComponent(0).description
+        //editedItem = ListItem(name: itemNameTextField.text!,quantity: Int(itemQuantityTextField.text!)!)
+        if let newName = editedItemName {
+            if let newQuantity = editedItemQuantity {
+                delegate!.doneEditing(chosenCategory, editedItemName: newName, editedItemQuantity: newQuantity, controller: self)
+            }
+        }
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- deliverables/week5/week5/ViewController.swift
+++ deliverables/week5/week5/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+protocol EditItemDelegate{
+    func doneEditing(chosenCategory: String, editedItemName : String, editedItemQuantity : Int, controller: EditItem)
+} //include EditItemDelegate
+
+protocol NewCategoryDelegate{
+    func doneAddingCategory(newCategory : String, controller: NewCategory)
+}
+
+protocol NewItemDelegate {
+    func doneAddingItem(nameOfItem : String, quantityOfItem : Int, itemCategory : String, controller : NewItem)
+}
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NewCategoryDelegate, NewItemDelegate, EditItemDelegate {
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     var shoppingList = ShoppingList()
 
     override func viewDidLoad() {
@@ -17,6 +34,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         shoppingList.addItem(ListItem(name: "milk", quantity: 2), category: "Grocery")
         shoppingList.addItem(ListItem(name: "bread", quantity: 3), category: "Grocery")
         shoppingList.addItem(ListItem(name: "broom", quantity: 1), category: "Household")
+
     }
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -45,4 +63,70 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
+    
+    func doneAddingCategory(newCategory: String, controller: NewCategory) {
+        shoppingList.addCategory(newCategory)
+        controller.navigationController!.popViewControllerAnimated(true)
+    }
+    
+    func doneAddingItem(nameOfItem: String, quantityOfItem: Int, itemCategory: String, controller: NewItem) {
+        shoppingList.addItem(ListItem(name: nameOfItem, quantity: quantityOfItem), category: itemCategory)
+        controller.navigationController!.popViewControllerAnimated(true)
+    }
+    
+    func doneEditing(chosenCategory: String, editedItemName: String, editedItemQuantity: Int, controller: EditItem) {
+        //need more code to add to shopping list, 
+        controller.navigationController!.popViewControllerAnimated(true)
+    }
+
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+       // var editItemScreen = segue.destinationViewController as? EditItem
+        if let cell = sender as? UITableViewCell, let destination = segue.destinationViewController as? EditItem {
+            let indexPath = tableView.indexPathForCell(cell)!
+            let category = shoppingList.sortedCategories()[indexPath.section]
+            let items = shoppingList.items[category]
+            destination.itemNameLabelText = items![indexPath.row].name
+            destination.itemQuantityLabelText = "Quantity is: \(items![indexPath.row].quantity)"
+            destination.itemCategoryLabelText = category
+            destination.categories = shoppingList.sortedCategories()
+            destination.delegate = self
+        }
+       /* if let addItemButton = sender as? UIBarButtonItem, let destination = segue.destinationViewController as? NewItem {
+            destination.categories = shoppingList.sortedCategories()
+        }*/
+        if segue.identifier == "newItem" {
+            let destination = segue.destinationViewController as? NewItem
+            destination!.categories = shoppingList.sortedCategories()
+            destination!.delegate = self
+        }
+
+        if segue.identifier == "newCategory" {
+            let destination = segue.destinationViewController as? NewCategory
+            destination!.delegate = self
+        }
+
+        
+        
+        /*if let indexPath = () {
+            editItemScreen.
+        }*/
+    
+
+
+    }
+// Get the new view controller using segue.destinationViewController.
+// Pass the selected object to the new view controller.
+    @IBAction func unwindToShoppingList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.sourceViewController as? NewCategory, newCategory = sourceViewController.newCategory {
+            shoppingList.addCategory(newCategory)
+            let newIndexPath = NSIndexPath(forRow: shoppingList.sortedCategories().count, inSection: 0 )
+
+            //tableView.insertSections(shoppingList.sortedCategories(), withRowAnimation: .Bottom)
+               // insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
+        }
+    }
+ 
 }
